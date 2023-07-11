de6747d55ea5a7078ce5bb2261ec36bf67e9a7ba
Diener Dornelas <diener.ld@outlook.com>
2023-07-11 20:36:01 -0300

    add components to export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,5 +4,6 @@ import '../colors.css'
 import DTypography from './components/DTypography'
 import DButton from './components/DButton'
 import DCard from './components/DCard'
+import DTextField from './components/DTextField'
 
-export { DButton, DCard, DTypography }
+export { DButton, DCard, DTypography, DTextField }
